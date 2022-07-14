2bf31b4edfcf8afa06d68f2e3f3994c37bb9aabf
duc-huy.nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2022-07-14 11:00:06 +0200

    done for the morning

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,112 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QToolButton>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    init();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::init() {
+    QVector<int> c = QVector<int>(3);
+    board.push_back(c);
+    board.push_back(c);
+    board.push_back(c);
+}
+
+void MainWindow::playerMove(QString name) {
+    QToolButton *btn = ui->gridWidget->findChild<QToolButton *>(name);
+    if(btn->icon().isNull()) {
+        btn->setIcon(QIcon(":/images/images/o.png"));
+        btn->setIconSize(QSize(150,150));
+
+        QChar c = name[name.size() - 1];
+        if(c == QChar('n')) {
+            c = '1';
+        }
+        int val  = c.digitValue() - 1;
+        int col = val / 3;
+        int lin = val % 3;
+        board[col][lin] = 1;
+        for(int i = 0; i < 3; i++) {
+                qDebug() << board[i][0] << " " << board[i][1] << " " << board[i][2];
+        }
+    }else {
+        return;
+    }
+}
+
+void MainWindow::aiMove() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+void MainWindow::on_toolButton_clicked()
+{
+    playerMove("toolButton");
+}
+
+void MainWindow::on_toolButton_2_clicked()
+{
+    playerMove("toolButton_2");
+}
+
+
+void MainWindow::on_toolButton_3_clicked()
+{
+    playerMove("toolButton_3");
+}
+
+
+void MainWindow::on_toolButton_4_clicked()
+{
+    playerMove("toolButton_4");
+}
+
+
+void MainWindow::on_toolButton_5_clicked()
+{
+    playerMove("toolButton_5");
+}
+
+void MainWindow::on_toolButton_6_clicked()
+{
+    playerMove("toolButton_6");
+}
+
+
+void MainWindow::on_toolButton_7_clicked()
+{
+    playerMove("toolButton_7");
+}
+
+
+void MainWindow::on_toolButton_8_clicked()
+{
+    playerMove("toolButton_8");
+}
+
+
+void MainWindow::on_toolButton_9_clicked()
+{
+    playerMove("toolButton_9");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,37 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_toolButton_clicked();
+
+    void on_toolButton_6_clicked();
+
+    void on_toolButton_2_clicked();
+
+    void on_toolButton_3_clicked();
+
+    void on_toolButton_4_clicked();
+
+    void on_toolButton_5_clicked();
+
+    void on_toolButton_7_clicked();
+
+    void on_toolButton_8_clicked();
+
+    void on_toolButton_9_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    //empty: 0, player:1, bot: 2
+    QVector<QVector<int>> board;
+
+    void init();
+
+
+
+    void playerMove(QString name);
+
+    void aiMove();
 };
 #endif // MAINWINDOW_H
